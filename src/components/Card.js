d272142e46bc11baf51d42e6291575539bab004d
Mayank Sethi <mayank@digibrain.co.in>
2024-04-15 02:14:31 +0530

    created card layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,30 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+import { HStack, Heading, Image, Text, VStack } from '@chakra-ui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
 
 const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <VStack backgroundColor={'white'} borderRadius={'lg'}>
+      <Image src={imageSrc} alt={title} objectFit="cover" borderRadius={'lg'} />
+      <VStack align="start" padding={4} spacing={4}>
+        <Heading as="h6" size="sm" color={'black'}>
+          {title}
+        </Heading>
+        <Text fontSize="sm" color={'black'}>
+          {description}
+        </Text>
+        <HStack color={'black'}>
+          <a href="#">
+            <Text fontSize="xs" color={'black'}>
+              {'See More'}
+            </Text>
+          </a>
+          <FontAwesomeIcon icon={faArrowRight} size="xs" />
+        </HStack>
+      </VStack>
+    </VStack>
+  );
 };
 
 export default Card;
