4586bcd004dbab57d94e5a00e7ee90822ec21f91
Mayank Sethi <mayank@digibrain.co.in>
2024-04-15 01:28:10 +0530

    minor cleanup

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Avatar, Heading, VStack } from '@chakra-ui/react';
 import FullScreenSection from './FullScreenSection';
 
-const avatarUrl = `https://i.pravatar.cc/150?img=7`;
-const greeting = 'Hello, I am Pete!';
-const bio1 = 'A frontend developer';
-const bio2 = 'specialised in React';
+const avatar = {
+  url: `https://i.pravatar.cc/150?img=7`,
+  greeting: `Hello, I am Pete!`,
+  bio1: `A frontend developer`,
+  bio2: `specialised in React`,
+};
 
 const LandingSection = () => (
   <FullScreenSection
@@ -16,17 +18,17 @@ const LandingSection = () => (
   >
     <VStack spacing={10}>
       <VStack>
-        <Avatar src={avatarUrl} size="xl" />
+        <Avatar src={avatar.url} size="xl" />
         <Heading as="h6" size="sm" noOfLines={1}>
-          {greeting}
+          {avatar.greeting}
         </Heading>
       </VStack>
       <VStack>
         <Heading as="h4" size="xl" noOfLines={1}>
-          {bio1}
+          {avatar.bio1}
         </Heading>
         <Heading as="h4" size="xl" noOfLines={1}>
-          {bio2}
+          {avatar.bio2}
         </Heading>
       </VStack>
     </VStack>
