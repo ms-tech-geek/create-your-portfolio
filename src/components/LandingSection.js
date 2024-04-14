ef3fae9892612d9766f1a56ad89d0438c06392be
Mayank Sethi <mayank@digibrain.co.in>
2024-04-15 01:26:32 +0530

    updated landing section

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,13 +1,12 @@
-import React from "react";
-import { Avatar, Heading, VStack } from "@chakra-ui/react";
-import FullScreenSection from "./FullScreenSection";
+import React from 'react';
+import { Avatar, Heading, VStack } from '@chakra-ui/react';
+import FullScreenSection from './FullScreenSection';
 
-const greeting = "Hello, I am Pete!";
-const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const avatarUrl = `https://i.pravatar.cc/150?img=7`;
+const greeting = 'Hello, I am Pete!';
+const bio1 = 'A frontend developer';
+const bio2 = 'specialised in React';
 
-// Implement the UI for the LandingSection component according to the instructions.
-// Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
   <FullScreenSection
     justifyContent="center"
@@ -15,7 +14,22 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+    <VStack spacing={10}>
+      <VStack>
+        <Avatar src={avatarUrl} size="xl" />
+        <Heading as="h6" size="sm" noOfLines={1}>
+          {greeting}
+        </Heading>
+      </VStack>
+      <VStack>
+        <Heading as="h4" size="xl" noOfLines={1}>
+          {bio1}
+        </Heading>
+        <Heading as="h4" size="xl" noOfLines={1}>
+          {bio2}
+        </Heading>
+      </VStack>
+    </VStack>
   </FullScreenSection>
 );
 
